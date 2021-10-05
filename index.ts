296775033da2a032b9d484f1386e574e5a38394d
Zachary Moore <zsmoore@zachary-moore.com>
2021-10-05 19:05:07 -0400

    Update to take in input (#1)

--- index.ts
+++ index.ts
@@ -1,4 +1,10 @@
 import compare from './lib/compare.js';
+import { readFileSync } from 'fs';
 
-compare();
+const args = process.argv.slice(2);
+const filePath1 = args[0];
+const filePath2 = args[1];
+const fileAsString1 = readFileSync(filePath1).toString();
+const fileAsString2 = readFileSync(filePath2).toString();
+compare(fileAsString1, fileAsString2);
 export default compare;
